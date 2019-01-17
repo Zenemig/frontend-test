74e400b11783e3f71014432f557a6115dd6efbf3
Zenemig <hola@zenemig.net>
2019-01-17 19:26:54 -0300

    feat(homepage): create route and acceptance test

---
+++ tests/acceptance/app-test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit'
+import { visit, currentURL, click } from '@ember/test-helpers'
+import { setupApplicationTest } from 'ember-qunit'
+
+module('Acceptance | app', function(hooks) {
+  setupApplicationTest(hooks)
+
+  test('Should show Homepage', async function(assert) {
+    await visit('/')
+
+    assert.equal(currentURL(), '/homepage', 'Should redirect automatically')
+  })
+})
