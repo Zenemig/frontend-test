ecb1493bfa8fd656218199886775631be73e3a7b
Zenemig <hola@zenemig.net>
2019-01-22 15:30:01 -0300

    test(top-header): fix integration test

--- tests/integration/components/top-header-test.js
+++ tests/integration/components/top-header-test.js
@@ -1,26 +1,24 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit'
+import { setupRenderingTest } from 'ember-qunit'
+import { render } from '@ember/test-helpers'
+import hbs from 'htmlbars-inline-precompile'
 
 module('Integration | Component | top-header', function(hooks) {
-  setupRenderingTest(hooks);
+  setupRenderingTest(hooks)
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    // Set any properties with this.set('myProperty', 'value')
+    // Handle any actions with this.set('myAction', function(val) { ... })
 
-    await render(hbs`{{top-header}}`);
+    await render(hbs`{{top-header}}`)
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(this.element.textContent.trim(), 'The Marketing Blog')
 
     // Template block usage:
     await render(hbs`
-      {{#top-header}}
-        template block text
-      {{/top-header}}
-    `);
+      {{#top-header}}{{/top-header}}
+    `)
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), 'The Marketing Blog')
   });
 });
