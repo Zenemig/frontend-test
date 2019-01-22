d9f8a9faa42a89546df4a917d1ec438fd37d74e4
Zenemig <hola@zenemig.net>
2019-01-22 15:35:31 -0300

    feat(homepage): create a hard-coded model, template and test for posts

--- app/routes/homepage.js
+++ app/routes/homepage.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route'
 
 export default Route.extend({
+  model() {
+    return [{
+      title: 'Post 1 Title'
+    },
+    {
+      title: 'Post 2 Title'
+    },
+    {
+      title: 'Post 3 Title'
+    }]
+  }
 })
--- tests/unit/routes/homepage-test.js
+++ tests/unit/routes/homepage-test.js
@@ -1,11 +1,17 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from 'qunit'
+import { visit } from '@ember/test-helpers'
+import { setupTest } from 'ember-qunit'
 
 module('Unit | Route | homepage', function(hooks) {
-  setupTest(hooks);
+  setupTest(hooks)
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:homepage');
-    assert.ok(route);
-  });
-});
+    let route = this.owner.lookup('route:homepage')
+    assert.ok(route)
+  })
+
+  test('should list posts.', async function(assert) {
+    await visit('/')
+    assert.equal(this.element.querySelectorAll('.c-post-preview').length, 3, 'should display 3 posts')
+  })
+})
